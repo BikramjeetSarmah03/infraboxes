410c11be605cd678210f360ab1d234d3a5c7ec4a
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2026-02-21 17:12:21 +0530

    biome errors

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import { GalleryVerticalEnd } from "lucide-react";
+import Link from "next/link";
 import type { PropsWithChildren } from "react";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
@@ -6,12 +7,12 @@ export default function AuthLayout({ children }: PropsWithChildren) {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <Link href="/" className="flex items-center gap-2 font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
             Acme Inc.
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">{children}</div>
@@ -20,7 +21,7 @@ export default function AuthLayout({ children }: PropsWithChildren) {
       <div className="bg-muted relative hidden lg:block">
         <img
           src="/placeholder.svg"
-          alt="Image"
+          alt="image-source"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
